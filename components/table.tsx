0dc81502ec78c91bc3d96949f72b83177763f1c4
BIGtanukiudon <rksakanatanuki@gmail.com>
2024-02-17 20:51:35 +0900

    feat: 削除ボタン追加 #5

--- components/table.tsx
+++ components/table.tsx
@@ -1,7 +1,7 @@
 import { useStorage } from "~hooks/storage"
 
 const Table: React.FC = () => {
-  const { borderPageColorizerStorage } = useStorage()
+  const { borderPageColorizerStorage, deleteBorderPageColorizer } = useStorage()
 
   return (
     <table className="table-auto border-collapse w-full text-base">
@@ -9,6 +9,7 @@ const Table: React.FC = () => {
         <tr>
           <th className="table-th">Domain Name</th>
           <th className="table-th">Border Color</th>
+          <th className="table-th"></th>
         </tr>
       </thead>
       <tbody className="bg-white text-left">
@@ -20,6 +21,13 @@ const Table: React.FC = () => {
                 <input type="color" value={item.color} disabled />
                 <br />
                 {item.color}
+              </td>
+              <td className="table-td">
+                <button
+                  onClick={() => deleteBorderPageColorizer(item.no)}
+                  className="remove-btn">
+                  Remove
+                </button>
               </td>
             </tr>
           ))
